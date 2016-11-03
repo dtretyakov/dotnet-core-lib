448aa7503cbec5ae92fa55ac344095ef1b1855c4
Dmitry.Tretyakov <dtretyakov@gmail.com>
2016-11-03 15:09:34 +0300

    Add nunit tests

---
+++ TestLibrary/NUnitTests/CalculatorTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NUnitTests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [TestCase(1, 1, 2)]
+        [TestCase(-1, -1, -2)]
+        [TestCase(100, 5, 105)]
+        public void CanAddNumbers(int x, int y, int expected)
+        {
+            Assert.That(x + y, Is.EqualTo(expected));
+        }
+
+        [TestCase(1, 1, 0)]
+        [TestCase(-1, -1, 0)]
+        [TestCase(100, 5, 95)]
+        public void CanSubtract(int x, int y, int expected)
+        {
+            Assert.That(x - y, Is.EqualTo(expected));
+        }
+
+        [TestCase(1, 1, 1)]
+        [TestCase(-1, -1, 1)]
+        [TestCase(100, 5, 500)]
+        public void CanMultiply(int x, int y, int expected)
+        {
+            Assert.That(x * y, Is.EqualTo(expected));
+        }
+
+        [TestCase(1, 1, 1)]
+        [TestCase(-1, -1, 1)]
+        [TestCase(100, 5, 20)]
+        public void CanDivide(int x, int y, int expected)
+        {
+            Assert.That(x / y, Is.EqualTo(expected));
+        }
+    }
+}
